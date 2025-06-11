0e7f87fa4c2e569b19783f20470e66543160a29a
KittyC513 <shuichunchenkitty@gmail.com>
2025-06-11 14:31:17 -0400

    Fix merge conflicts

--- Assets/Scripts/DialogueSystem/NormalNPCs/UIControl_NormalNPCs.cs
+++ Assets/Scripts/DialogueSystem/NormalNPCs/UIControl_NormalNPCs.cs
@@ -8,8 +8,6 @@ using static PixelCrushers.DialogueSystem.StartConversationOnDialogueEvent;
 
 public class UIControl_NormalNPCs : MonoBehaviour
 {
-    //public PixelCrushers.DialogueSystem.Usable usable;
-    public PixelCrushers.DialogueSystem.DialogueSystemController dialogueController;
     public PixelCrushers.DialogueSystem.ProximitySelector proximitySelector;
     public Camera cam;
     public Rect camRect_P1;
@@ -19,7 +17,6 @@ public class UIControl_NormalNPCs : MonoBehaviour
     public PlayerInputDetection playerInputDetection;
 
     public PixelCrushers.DialogueSystem.DialogueSystemTrigger dialogueSystemTrigger;
-    public PlayerStateMachine playerStateMachine;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -59,7 +56,7 @@ public class UIControl_NormalNPCs : MonoBehaviour
     /// </summary>
     public void SwitchToConversantCamRect()
     {
-        if (proximitySelector.usablesInRange != null && playerInputDetection.crouchPressed)
+        if (proximitySelector.usablesInRange != null && playerInputDetection.attackPressed)
         {
             if (this.gameObject.layer == LayerMask.NameToLayer("Player1"))
             {
@@ -69,7 +66,6 @@ public class UIControl_NormalNPCs : MonoBehaviour
                 Debug.Log("Player1cam's rect changed");
                 playerManager.p1cam.rect = camRect_P1;
                 playerManager.p2cam.rect = new Rect(camRect_P1.width, 0, 1 - camRect_P1.width, 1);
-                playerManager.players[0].GetComponent<PlayerStateMachine>().UnFreezeStateMachine();
                 playerManager.startConversation_p1 = true;
             }
 
@@ -81,7 +77,6 @@ public class UIControl_NormalNPCs : MonoBehaviour
 
                 playerManager.p2cam.rect = camRect_P2;
                 playerManager.p1cam.rect = new Rect(0, 0, 1 - camRect_P2.width, 1);
-                playerManager.players[1].GetComponent<PlayerStateMachine>().UnFreezeStateMachine();
                 playerManager.startConversation_p2 = true;
             }
             //print("ButtonDown" + proximitySelector.IsUseButtonDown());
