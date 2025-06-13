e7302bd924fe589fa6bb56bd311578f8062ce4e1
Chummy-Guy <78655854+Chummy-Guy@users.noreply.github.com>
2025-06-13 17:35:47 -0400

    Reworked badge lists
    Moved badge lists from ScriptableObjects to a singleton as the main purpose of the badge list is to remember which badges were or were not earned

--- Assets/Scripts/Badges/BadgeManager.cs
+++ Assets/Scripts/Badges/BadgeManager.cs
@@ -3,47 +3,46 @@ using System.Collections.Generic;
 using UnityEditor.Rendering;
 using UnityEngine;
 
-//player will complete a task and then add a badge to their count of badges
-//2 lists, 1 is per level and 1 is per entire game to keep track of all earned badges
-//each level will have a set of badges to earn, when a badge is earned regardless of who earned it
-//we need to keep track of that in a counter outside of the level
-
-//badges won't have a numbered placement
-//will just be added as list
-//can only be earned once by each player
-
-//need a database somewhere that keeps track of all earned badges
-//a level by level badge manager can keep track of who has earned what badges
-//a static list 1 for each player is fine
-
-//need a way to check all earnable badges and keep track of if they were earned (for completionists)
-//could manually fill a list on starting a level
-//levels will need to have the information before the player even plays them
-
-//could have a list of earnable badges for each level (they would be numbered) and
-//whenever we need to trigger the earning of a badge it could say earn badge (level, badgeNum, player) 
-//need a static list of badges per level which can be held in a scriptable object
+//an in level script that keeps track of what badges were earned in this level specifically
+//can be used to display ui, reward the player with money or other things
+//this will reset upon reloading a level unlike the BadgeTracker which is a permanent script that remembers what was earned or not
 public class BadgeManager : MonoBehaviour
 {
-    [SerializeField] public BadgeList badgeList; //the badges earnable in this level
+    [SerializeField] public int levelID; //what index in the array of all levels badges should this level contain
+    private BadgeTracker badgeTracker; //the badges earnable in this level
 
     public List<Badge> player1Badges; //the badges earned by each player
     public List<Badge> player2Badges;
+    LevelBadgeList levelBadgeList;
 
+    private void Start()
+    {
+        badgeTracker = BadgeTracker.instance;
+
+        //set the earnable badges for this level
+        levelBadgeList = badgeTracker.badgeLists[levelID];
+    }
     public void EarnBadge(int badgeNum, int player)
     {
-        BadgeInfo targetBadge = badgeList.badgeList[badgeNum];
-        switch (player)
+        //target badge is the info containing a badge and if its completed or not
+        BadgeList targetBadge = levelBadgeList.badgeList[badgeNum];
+
+        //check if that badge number exists 
+        if (targetBadge != null )
         {
-            case 1:
-                player1Badges.Add(targetBadge.badge);
-                break;
-            case 2:
-                player2Badges.Add(targetBadge.badge);
-                break;
+            //add the badge to the player and tick it as earned in the badge tracker
+            switch (player)
+            {
+                case 1:
+                    player1Badges.Add(targetBadge.badge);
+                    break;
+                case 2:
+                    player2Badges.Add(targetBadge.badge);
+                    break;
+            }
 
+            targetBadge.earned = true;
         }
-
-        targetBadge.earned = true;
+     
     }
 }
--- Assets/Scripts/Badges/BadgeTracker.cs
+++ Assets/Scripts/Badges/BadgeTracker.cs
@@ -1,8 +1,37 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public class BadgeTracker
+//a single object that remembers and keeps track of what badges were earned in each level
+//also provides a list of earnable badges for each level
+public class BadgeTracker : MonoBehaviour
 {
     //update and keep track of what badges were earned
+    public static BadgeTracker instance;
+    public LevelBadgeList[] badgeLists;
 
-    //
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        instance = this;
+    }
+
+}
+
+[System.Serializable]
+public class BadgeList
+{
+    public Badge badge;
+    public bool earned = false;
+}
+
+[System.Serializable]
+public class LevelBadgeList
+{
+    public string levelName;
+    public BadgeList[] badgeList;
 }
