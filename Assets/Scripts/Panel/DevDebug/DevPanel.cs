117e1405991f0afc3902c13219aa0168bee3b02b
KittyC513 <shuichunchenkitty@gmail.com>
2025-06-24 16:09:29 -0400

    Added DevMode
    - Dev Mode are allowed to activate
    - Dev function : Enter Level 1

---
+++ Assets/Scripts/Panel/DevDebug/DevPanel.cs
@@ -0,0 +1,45 @@
+using PixelCrushers.DialogueSystem;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DevPanel : MonoBehaviour
+{
+    public UnityEngine.UI.Toggle toggle_on;
+    public UnityEngine.UI.Toggle toggle_off;
+    public GameObject DevFunctionPanel;
+    public Button btn_level1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        toggle_on.onValueChanged.AddListener((b) =>
+        {
+            if (b)
+            {
+                DevFunctionPanel.SetActive(true);
+            }
+        });
+
+        toggle_off.onValueChanged.AddListener((b) =>
+        {
+            if(b)
+            {
+                DevFunctionPanel.SetActive(false);
+            }
+        });
+
+        btn_level1.onClick.AddListener(() =>
+        {
+            EvenData.isAcceptedMission_lalah = true;
+            SceneManager.LoadScene("Level1");
+        });
+
+        DevFunctionPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
