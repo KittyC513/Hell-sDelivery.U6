5ea387b5f220505b29d4e665e44f0625841ddc0c
KittyC513 <shuichunchenkitty@gmail.com>
2025-06-26 16:37:37 -0400

    Created Player icon while selecting a character

--- Assets/Scripts/UIPanel/UGUI/CharacterSelectionPanel/CharacterSelectionPanel.cs
+++ Assets/Scripts/UIPanel/UGUI/CharacterSelectionPanel/CharacterSelectionPanel.cs
@@ -19,11 +19,20 @@ public class CharacterSelectionPanel : MonoBehaviour
     public bool onReady = false;
     public Image img_start;
 
+    public GameObject imgLeft_p1;
+    public GameObject imgLeft_p2;
+
+    public GameObject imgRight_p1;
+    public GameObject imgRight_p2;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         img_start.gameObject.SetActive(false);
+
+        leftIsSelected_p1 = true;
+        leftIsSelected_p2 = false;
     }
 
     // Update is called once per frame
@@ -44,6 +53,8 @@ public class CharacterSelectionPanel : MonoBehaviour
             lastInputTime_p1 = Time.time;
             print("Left is selected for Player 1");
             print("right is selected for Player 2");
+
+
         }
 
         if (GameManager.instance.player1.GetComponent<PlayerInputDetection>().GetHorizontalMovement().x > 0.5)
@@ -51,6 +62,7 @@ public class CharacterSelectionPanel : MonoBehaviour
             leftIsSelected_p1 = false;
             lastInputTime_p1 = Time.time;
         }
+
 
     }
 
@@ -61,8 +73,6 @@ public class CharacterSelectionPanel : MonoBehaviour
         {
             leftIsSelected_p2 = true;
             lastInputTime_p2 = Time.time;
-            print("Left is selected for Player 2");
-            print("right is selected for Player 1");
         }
 
         if (GameManager.instance.player2.GetComponent<PlayerInputDetection>().GetHorizontalMovement().x > 0.5f)
@@ -99,6 +109,8 @@ public class CharacterSelectionPanel : MonoBehaviour
             onReady = false;
         }
 
+        SelectedIconControl();
+
     }
 
     public void OnReady()
@@ -131,5 +143,14 @@ public class CharacterSelectionPanel : MonoBehaviour
         {
             img_start.gameObject.SetActive(false);
         }
+    }
+
+    public void SelectedIconControl()
+    {
+        imgLeft_p1.SetActive(leftIsSelected_p1);
+        imgRight_p1.SetActive(!leftIsSelected_p1);
+
+        imgLeft_p2.SetActive(leftIsSelected_p2);
+        imgRight_p2.SetActive(!leftIsSelected_p2);
     }
 }
