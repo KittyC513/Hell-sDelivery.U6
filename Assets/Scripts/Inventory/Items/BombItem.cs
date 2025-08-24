d94ae1ba5e79ad8249feb903dc43049b188cc0d0
KittyC513 <shuichunchenkitty@gmail.com>
2025-08-24 11:31:08 -0400

    Bug Fix: Players could throw bombs while in a free-fall state
    - Added ValidatedCheck() method to the BombItem script

--- Assets/Scripts/Inventory/Items/BombItem.cs
+++ Assets/Scripts/Inventory/Items/BombItem.cs
@@ -22,34 +22,21 @@ public class BombItem : ItemBase
 
     public List<BombMovement> bombsList = new List<BombMovement>();
 
-    //protected override void Awake()
-    //{
-    //    base.Awake();
-    //    //itemHandler = GetComponent<ItemHandler>();
-    //    //iHandler = GetComponent<ItemHandler>();
-    //    //if (iHandler == null) Debug.Log(this.ToString() + " needs an itemHandler attached on the gameObject " + this.gameObject.name); 
-    //}
+    public PlayerStateMachine pStateMachine;
+    public bool validatedUse = false;
 
-    //In Start method
-    //public override void Initialize()
-    //{
-    //    //itemHandler.onItemTrigger += ThrowBomb;
-    //    print("BombItem initialized and ready to use");
-    //}
-
-    //private void OnDisable()
-    //{
-    //    //itemHandler.onItemTrigger -= ThrowBomb; 
-    //}
 
     public override void UseFunction()
     {
-        ThrowBomb();
-        print("Bomb is thrown");
+        if (validatedUse)
+        {
+            ThrowBomb();
+            print("Bomb is thrown");
+        }
     }
     private void ThrowBomb()
     {
-        if(numOfBombs < maxBombs)
+        if (numOfBombs < maxBombs)
         {
             if (timer >= cdSpawn)
             {
@@ -62,7 +49,7 @@ public class BombItem : ItemBase
                 //if (Vector3.Distance(bombObj.transform.position, this.transform.position) < 0.1f)
                 BombMovement bombMovement = bombObj.GetComponent<BombMovement>();
                 bombMovement.playerLockOn = this.transform.parent.GetComponent<PlayerLockOn>();
-                
+
                 if (bombMovement.playerLockOn.lockTarget != null)
                     bombMovement.targetPos = bombMovement.playerLockOn.lockTarget.transform;
 
@@ -85,34 +72,34 @@ public class BombItem : ItemBase
         {
             timer = cdSpawn;
         }
-        
+     
+    }
 
-        ////When the item is equipped, the item will follow player's pos
-        //if (itemHandler.equipped)
-        //{
-        //    /**************************************/
-        //    /**************************************/
-        //    //Set object as a child of a player 
-
-
-        //    //parentObj = iHandler.iControl.gameObject.transform.parent;
-        //    //this.transform.position = parentObj.transform.position;
-
-        //    if (Vector3.Distance(this.transform.position, itemHandler.iControl.transform.position + offset) < 0.1f)
-        //        this.transform.SetParent(itemHandler.iControl.transform);
-        //    else
-        //        this.transform.position = itemHandler.iControl.transform.position + offset;
-        //    //Debug.Log("Parent");
-
-        //    /**************************************/
-        //    /**************************************/
-        //}
-
-        //if (!itemHandler.equipped && (worldCollider.enabled == false || itemHandler.rb.useGravity == false))
-        //{
-        //    worldCollider.enabled = true;
-        //    itemHandler.rb.useGravity = true;
-        //}
-
+    protected override void Update()
+    {
+        base.Update();
+        ValidatedCheck();
+    }
+    private void ValidatedCheck()
+    {
+        if (isOnUse)
+        {
+            if(pStateMachine != null)
+            {
+                if (pStateMachine.showCurrentState == PlayerStateMachine.PlayerStates.freeFall)
+                    validatedUse = false;
+                else
+                    validatedUse = true;
+            }
+            else
+            {
+                pStateMachine = this.transform.parent.GetComponent<PlayerStateMachine>();
+            }
+        }
+        else
+        {
+            validatedUse = false;
+            pStateMachine = null;
+        }
     }
 }
