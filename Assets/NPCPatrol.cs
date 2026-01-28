9fac0560243552dec601fd412e40bfc4e80fa2b8
kateewhy001 <138513011+kateewhy001@users.noreply.github.com>
2026-01-28 16:47:52 -0500

    edited dialogue ui

---
+++ Assets/NPCPatrol.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class NPCPatrol : MonoBehaviour
+{
+    public GameObject patrolPoint1;
+    public GameObject patrolPoint2;
+    public GameObject patrolPoint3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GoToPatrolPoints();
+    }
+
+    public void GoToPatrolPoints()
+    {
+        
+    }
+}
